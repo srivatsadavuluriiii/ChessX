2ba88e1304b3ec5d09b54d909a77d13bfb53aef8
srivatsadavuluriiii <xstcy@protonmail.com>
2024-03-13 21:00:06 +0530

    Initial Commit

---
+++ ChessX/ChessXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessXApp.swift
+//  ChessX
+//
+//  Created by srivatsa davuluri on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ChessXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChessX/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChessX
+//
+//  Created by srivatsa davuluri on 13/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
